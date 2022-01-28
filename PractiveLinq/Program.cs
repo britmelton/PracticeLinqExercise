b931293210e698101c8a5955d690f320de81336c
brittany melton <brittanymelton26@gmail.com>
2022-01-28 15:54:54 -0500

    video games list ordered by length of game name

---
+++ PractiveLinq/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PractiveLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Order the list of games by length of the game name.
+
+            var videoGames = new List<string>()
+            { "mario kart", "mario party superstars", "donkey kong", "mortal kombat", "sonic the hedgehog" };
+            
+            var nameLength = videoGames.OrderBy(name => name.Length).ThenBy(name => name);
+            
+            foreach (var name in nameLength)
+                Console.WriteLine(name);
+
+            Console.WriteLine();
+
+            var namefilter = videoGames.Where(name => name.Contains("k"));
+            foreach (var name in namefilter)
+                Console.WriteLine(name);
+       
+
+            Console.WriteLine($"\n{videoGames.Count()}");
+
+           
+           
+
+
+
+
+
+
+
+        }
+    }
+}
